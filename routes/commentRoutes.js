b132c9dd2a90a7957aa480bc4786511672740c22
xxalrs <Kostikkostjuk@gmail.com>
2026-04-20 16:40:52 +0300

    Lab 7 - Update routes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
+const validate = require('../middlewares/validate');
+const commentV = require('../middlewares/validators/commentValidator');
 
-router.post('/', commentController.createComment);
-router.get('/post/:postId', commentController.getCommentsByPost);
-router.put('/:id', commentController.updateComment);
-router.delete('/:id', commentController.deleteComment);
+router.post('/', commentV.createCommentRules, validate, commentController.createComment);
+router.get('/post/:postId', commentV.postIdParamRules, validate, commentController.getCommentsByPost);
+router.put('/:id', commentV.updateCommentRules, validate, commentController.updateComment);
+router.delete('/:id', commentV.deleteCommentRules, validate, commentController.deleteComment);
 
 module.exports = router;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
+const validate = require('../middlewares/validate');
+const postV = require('../middlewares/validators/postValidator');
 
-router.post('/', postController.createPost);
-router.get('/', postController.getAllPosts);
-router.get('/search', postController.searchPosts);
-router.get('/:id', postController.getPostById);
-router.put('/:id', postController.updatePost);
-router.patch('/:id/like', postController.likePost);
-router.delete('/:id', postController.deletePost);
+router.post('/', postV.createPostRules, validate, postController.createPost);
+router.get('/', postV.getPostsRules, validate, postController.getAllPosts);
+router.get('/search', postV.searchPostsRules, validate, postController.searchPosts);
+router.get('/:id', postV.mongoIdParamRule, validate, postController.getPostById);
+router.put('/:id', postV.updatePostRules, validate, postController.updatePost);
+router.patch('/:id/like', postV.mongoIdParamRule, validate, postController.likePost);
+router.delete('/:id', postV.mongoIdParamRule, validate, postController.deletePost);
 
 module.exports = router;
