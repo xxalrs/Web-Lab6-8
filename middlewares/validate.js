ed8adaef97746371dfae2f941025e6cf6bdbd640
xxalrs <Kostikkostjuk@gmail.com>
2026-04-20 16:00:37 +0300

    Lab 7 - Add global validation

---
+++ middlewares/validate.js
@@ -0,0 +1,14 @@
+const { validationResult } = require('express-validator');
+
+module.exports = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      message: 'Validation error',
+      errors: errors.array().map(e => ({ field: e.path, msg: e.msg })),
+      statusCode: 400
+    });
+  }
+  next();
+};
